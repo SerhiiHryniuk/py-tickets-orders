175dd25a38671109576e97e4f074a4503fb3d9ab
Serhii Hryniuk <serhiibhryniuk@gmail.com>
2026-06-02 15:45:57 +0300

    fix: add file

--- cinema/views.py
+++ cinema/views.py
@@ -121,7 +121,6 @@ class OrderPagination(PageNumberPagination):
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    pagination_class = OrderPagination
 
     def get_queryset(self):
         queryset = self.queryset.filter(user=self.request.user)
